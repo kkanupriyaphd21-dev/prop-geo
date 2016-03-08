b7082b2254145a7f4c550bbf9bcb546e9ce32998
Vishav Sharma <vishav.sharma@gmail.com>
2016-03-08 06:21:12 -0700

    chore: bulk update
    Includes changes up to original commit b7368f6c

--- build.sh
+++ build.sh
@@ -2,9 +2,15 @@
 set -e
 
 VERSION="0.0.1"
-BUILD_TIME=$(date +%FT%T%z)
-GIT_SHA=$(git rev-parse --short HEAD)
-LDFLAGS="-X github.com/tidwall/propgeo/core.Version=${VERSION} -X github.com/tidwall/propgeo/core.BuildTime=${BUILD_TIME} -X github.com/tidwall/propgeo/core.GitSHA=${GIT_SHA}"
+PROTECTED_MODE="no"
+
+# Hardcode some values to the core package
+LDFLAGS="$LDFLAGS -X github.com/tidwall/propgeo/core.Version=${VERSION}"
+LDFLAGS="$LDFLAGS -X github.com/tidwall/propgeo/core.GitSHA=$(git rev-parse --short HEAD)"
+LDFLAGS="$LDFLAGS -X github.com/tidwall/propgeo/core.BuildTime=$(date +%FT%T%z)"
+if [ "$PROTECTED_MODE" == "no" ]; then
+	LDFLAGS="$LDFLAGS -X github.com/tidwall/propgeo/core.ProtectedMode=no"
+fi
 
 export GO15VENDOREXPERIMENT=1
 
@@ -32,7 +38,7 @@ if [ "$NOCOPY" != "1" ]; then
 	cd $WD
 fi
 
-core/gen.sh
+#core/gen.sh
 
 # build and store objects into original directory.
 go build -ldflags "$LDFLAGS" -o "$OD/propgeo-server" cmd/propgeo-server/*.go
--- controller/server/server.go
+++ controller/server/server.go
@@ -24,17 +24,17 @@ this mode connections are only accepted from the loopback interface. If you
 want to connect from external computers to PropGeo you may adopt one of the
 following solutions: 
 
-1) Disable protected mode sending the command 'CONFIG SET protected-mode no'
+1) Disable protected mode by sending the command 'CONFIG SET protected-mode no'
    from the loopback interface by connecting to PropGeo from the same host 
    the server is running, however MAKE SURE PropGeo is not publicly accessible
    from internet if you do so. Use CONFIG REWRITE to make this change
    permanent. 
 2) Alternatively you can just disable the protected mode by editing the PropGeo
-   configuration file, and setting the protected mode option to 'no', and then
-   restarting the server. 
+   configuration file, and setting the 'protected-mode' option to 'no', and
+   then restarting the server.
 3) If you started the server manually just for testing, restart it with the
    '--protected-mode no' option. 
-4) Setup a host address or an authentication password. 
+4) Use a host address or an authentication password. 
 
 NOTE: You only need to do one of the above things in order for the server 
 to start accepting connections from the outside.
@@ -73,19 +73,19 @@ func handleConn(
 	protected func() bool,
 	handler func(conn *Conn, command []byte, rd *bufio.Reader, w io.Writer, websocket bool) error,
 ) {
+	addr := conn.RemoteAddr().String()
 	if core.ShowDebugMessages {
-		addr := conn.RemoteAddr().String()
 		log.Debugf("opened connection: %s", addr)
 		defer func() {
 			log.Debugf("closed connection: %s", addr)
 		}()
-		if !strings.HasPrefix(addr, "127.0.0.1:") && !strings.HasPrefix(addr, "[::1]:") {
-			if protected() {
-				// This is a protected server. Only loopback is allowed.
-				conn.Write(deniedMessage)
-				conn.Close()
-				return
-			}
+	}
+	if !strings.HasPrefix(addr, "127.0.0.1:") && !strings.HasPrefix(addr, "[::1]:") {
+		if protected() {
+			// This is a protected server. Only loopback is allowed.
+			conn.Write(deniedMessage)
+			conn.Close()
+			return
 		}
 	}
 	defer conn.Close()
