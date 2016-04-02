90bf0e6da7c1fe0e784bfed6e634ba95ad175e5a
Josh Baker <joshbaker77@gmail.com>
2016-04-02 14:13:20 -0700

    fixed shared string issue

--- controller/live.go
+++ controller/live.go
@@ -8,7 +8,6 @@ import (
 	"net"
 	"sync"
 
-	"github.com/tidwall/propgeo/client"
 	"github.com/tidwall/propgeo/controller/log"
 	"github.com/tidwall/propgeo/controller/server"
 )
@@ -139,7 +138,7 @@ func (c *Controller) goLive(inerr error, conn net.Conn, rd *server.AnyReaderWrit
 	var livemsg []byte
 	switch outputType {
 	case server.JSON:
-		livemsg = []byte(client.LiveJSON)
+		livemsg = []byte(`{"ok":true,"live":true}`)
 	case server.RESP:
 		livemsg = []byte("+OK\r\n")
 	}
