bf0dc33538f85302be2f7e1a7ba87ce451be8428
Vishav Sharma <vishav.sharma@gmail.com>
2016-06-08 21:58:10 +0300

    chore: bulk update
    Includes changes up to original commit a98e5c24

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="1.1.4"
+VERSION="1.2.0"
 PROTECTED_MODE="no"
 
 # Hardcode some values to the core package
@@ -93,6 +93,7 @@ if [ "$1" == "package" ]; then
 	package "Windows" "windows" "amd64"
 	package "Mac" "darwin" "amd64"
 	package "Linux" "linux" "amd64"
+	package "FreeBSD" "freebsd" "amd64"
 	exit
 fi
 
