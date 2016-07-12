3700b0c43f966cdbb381bcea766d8f98b121c981
Rohini Das <rohini.das@gmail.com>
2016-07-12 13:29:52 -0600

    chore: bulk update
    Includes changes up to original commit c99beb57

--- controller/search.go
+++ controller/search.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/tidwall/resp"
 	"github.com/tidwall/propgeo/controller/bing"
+	"github.com/tidwall/propgeo/controller/glob"
 	"github.com/tidwall/propgeo/controller/server"
 	"github.com/tidwall/propgeo/geojson"
 	"github.com/tidwall/propgeo/geojson/geohash"
@@ -230,7 +231,7 @@ func (c *Controller) cmdSearchArgs(cmd string, vs []resp.Value, types []string) 
 			err = errInvalidNumberOfArguments
 			return
 		}
-		s.roam.pattern = globIsGlob(s.roam.id)
+		s.roam.pattern = glob.IsGlob(s.roam.id)
 		var smeters string
 		if vs, smeters, ok = tokenval(vs); !ok || smeters == "" {
 			err = errInvalidNumberOfArguments
