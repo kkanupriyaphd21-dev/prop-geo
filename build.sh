7d05032b10bc5b4dbb4174a871cc5d1f8d4f8a33
tidwall <joshbaker77@gmail.com>
2018-10-29 08:24:35 -0700

    Force disable cgo

--- build.sh
+++ build.sh
@@ -163,11 +163,13 @@ fi
 # generate the core package
 core/gen.sh
 
+export CGO_ENABLED=0
+
 # build and store objects into original directory.
-CGO_ENABLED=0 go build -ldflags "$LDFLAGS -extldflags '-static'" -o "$OD/propgeo-server" cmd/propgeo-server/*.go
-CGO_ENABLED=0 go build -ldflags "$LDFLAGS -extldflags '-static'" -o "$OD/propgeo-cli" cmd/propgeo-cli/*.go
-CGO_ENABLED=0 go build -ldflags "$LDFLAGS -extldflags '-static'" -o "$OD/propgeo-benchmark" cmd/propgeo-benchmark/*.go
-CGO_ENABLED=0 go build -ldflags "$LDFLAGS -extldflags '-static'" -o "$OD/propgeo-luamemtest" cmd/propgeo-luamemtest/*.go
+go build -ldflags "$LDFLAGS -extldflags '-static'" -o "$OD/propgeo-server" cmd/propgeo-server/*.go
+go build -ldflags "$LDFLAGS -extldflags '-static'" -o "$OD/propgeo-cli" cmd/propgeo-cli/*.go
+go build -ldflags "$LDFLAGS -extldflags '-static'" -o "$OD/propgeo-benchmark" cmd/propgeo-benchmark/*.go
+go build -ldflags "$LDFLAGS -extldflags '-static'" -o "$OD/propgeo-luamemtest" cmd/propgeo-luamemtest/*.go
 
 # test if requested
 if [ "$1" == "test" ]; then
