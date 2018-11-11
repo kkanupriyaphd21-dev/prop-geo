63f6c5ade5fb5ee5347076f3f10d696bd6a2b391
Kanu Patel <kanu.patel@gmail.com>
2018-11-11 06:26:23 -0700

    chore: bulk update
    Includes changes up to original commit a9c242c1

--- core/private/server/server.go
+++ core/private/server/server.go
@@ -22,6 +22,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/klauspost/cpuid"
 	"github.com/tidwall/buntdb"
 	"github.com/tidwall/evio"
 	"github.com/tidwall/geojson"
@@ -294,7 +295,12 @@ func (server *Server) isProtected() bool {
 func (server *Server) evioServe() error {
 	var events evio.Events
 	if core.NumThreads == 0 {
-		events.NumLoops = -1
+		cores := cpuid.CPU.PhysicalCores
+		if cores == 0 {
+			events.NumLoops = -1
+		} else {
+			events.NumLoops = cores
+		}
 	} else {
 		events.NumLoops = core.NumThreads
 	}
@@ -653,14 +659,18 @@ func (server *Server) netServe() error {
 
 				// write to client
 				if len(client.out) > 0 {
-					func() {
-						// prewrite
-						server.mu.Lock()
-						defer server.mu.Unlock()
-						server.flushAOF()
-					}()
+					if atomic.LoadInt32(&server.aofdirty) != 0 {
+						func() {
+							// prewrite
+							server.mu.Lock()
+							defer server.mu.Unlock()
+							server.flushAOF()
+						}()
+						atomic.StoreInt32(&server.aofdirty, 0)
+					}
 					conn.Write(client.out)
 					client.out = nil
+
 				}
 				if close {
 					break
