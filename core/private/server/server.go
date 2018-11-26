f36ba21133ae4396eea3a9d8817e5fc9db854315
Vishav Sharma <vishav.sharma@gmail.com>
2018-11-26 13:24:15 -0700

    chore: bulk update
    Includes changes up to original commit f9ab9ecf

--- core/private/server/server.go
+++ core/private/server/server.go
@@ -22,6 +22,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/tidwall/boxtree/d2"
 	"github.com/tidwall/buntdb"
 	"github.com/tidwall/evio"
 	"github.com/tidwall/geojson"
@@ -38,26 +39,26 @@ import (
 
 var errOOM = errors.New("OOM command not allowed when used memory > 'maxmemory'")
 
-const goingLive = "going live"
+const (
+	goingLive     = "going live"
+	hookLogPrefix = "hook:log:"
+)
 
-const hookLogPrefix = "hook:log:"
-
-type commandDetailsT struct {
-	command   string
-	key, id   string
-	field     string
-	value     float64
-	obj       geojson.Object
-	fields    []float64
-	fmap      map[string]int
-	oldObj    geojson.Object
-	oldFields []float64
-	updated   bool
-	timestamp time.Time
-
-	parent   bool               // when true, only children are forwarded
-	pattern  string             // PDEL key pattern
-	children []*commandDetailsT // for multi actions such as "PDEL"
+// commandDetails is detailed information about a mutable command. It's used
+// for geofence formulas.
+type commandDetails struct {
+	command   string            // client command, like "SET" or "DEL"
+	key, id   string            // collection key and object id of object
+	fmap      map[string]int    // map of field names to value indexes
+	obj       geojson.Object    // new object
+	fields    []float64         // array of field values
+	oldObj    geojson.Object    // previous object, if any
+	oldFields []float64         // previous object field values
+	updated   bool              // object was updated
+	timestamp time.Time         // timestamp when the update occured
+	parent    bool              // when true, only children are forwarded
+	pattern   string            // PDEL key pattern
+	children  []*commandDetails // for multi actions such as "PDEL"
 }
 
 // Server is a propgeo controller
@@ -101,15 +102,16 @@ type Server struct {
 
 	follows    map[*bytes.Buffer]bool
 	fcond      *sync.Cond
-	lstack     []*commandDetailsT
+	lstack     []*commandDetails
 	lives      map[*liveBuffer]bool
 	lcond      *sync.Cond
-	fcup       bool                        // follow caught up
-	fcuponce   bool                        // follow caught up once
-	shrinking  bool                        // aof shrinking flag
-	shrinklog  [][]string                  // aof shrinking log
-	hooks      map[string]*Hook            // hook name
-	hookcols   map[string]map[string]*Hook // col key
+	fcup       bool             // follow caught up
+	fcuponce   bool             // follow caught up once
+	shrinking  bool             // aof shrinking flag
+	shrinklog  [][]string       // aof shrinking log
+	hooks      map[string]*Hook // hook name
+	hookTree   d2.BoxTree       // hook spatial tree containing all
+	hooksOut   map[string]*Hook // hooks with "outside" detection
 	aofconnM   map[net.Conn]bool
 	luascripts *lScriptMap
 	luapool    *lStatePool
@@ -138,7 +140,7 @@ func Serve(host string, port int, dir string, http bool) error {
 		lives:    make(map[*liveBuffer]bool),
 		lcond:    sync.NewCond(&sync.Mutex{}),
 		hooks:    make(map[string]*Hook),
-		hookcols: make(map[string]map[string]*Hook),
+		hooksOut: make(map[string]*Hook),
 		aofconnM: make(map[net.Conn]bool),
 		expires:  make(map[string]map[string]time.Time),
 		started:  time.Now(),
@@ -1053,7 +1055,7 @@ func (server *Server) reset() {
 }
 
 func (server *Server) command(msg *Message, client *Client) (
-	res resp.Value, d commandDetailsT, err error,
+	res resp.Value, d commandDetails, err error,
 ) {
 	switch msg.Command() {
 	default:
