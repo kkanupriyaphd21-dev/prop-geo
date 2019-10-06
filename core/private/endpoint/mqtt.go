ce4812d773aec4633c6e0776ba31b1197866ddd2
Plamen Todorov <plamen@debimac.quint8.net>
2019-10-06 22:08:43 +0300

    MQTT clientId should be unique
    Each mqtt hook establishes separate connection to the MQTT broker. If
    their clientIds are all equal the MQTT broker will disconnect the clients - the
    protocol does not allow 2 connected clients with the same name

--- core/private/endpoint/mqtt.go
+++ core/private/endpoint/mqtt.go
@@ -83,7 +83,9 @@ func (conn *MQTTConn) Send(msg string) error {
 			}
 			ops = ops.SetTLSConfig(&config)
 		}
-		ops = ops.SetClientID("propgeo").AddBroker(uri)
+		nano := time.Now().UnixNano()
+		clientID := fmt.Sprintf("propgeo_%x", nano) //the id of connected clients should be unique
+		ops = ops.SetClientID(clientID).AddBroker(uri)
 		c := paho.NewClient(ops)
 
 		if token := c.Connect(); token.Wait() && token.Error() != nil {
