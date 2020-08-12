2c8b217a0682bf5d7e0cb027c39070910d7fdb33
Kanu Patel <kanu.patel@gmail.com>
2020-08-12 12:38:35 -0700

    chore: bulk update
    Includes changes up to original commit 8f9d45ed

--- core/private/server/monitor.go
+++ core/private/server/monitor.go
@@ -39,22 +39,24 @@ func (s *Server) liveMonitor(conn net.Conn, rd *PipelineReader, msg *Message) er
 	s.monconnsMu.Lock()
 	conn.Write([]byte("+OK\r\n"))
 	s.monconnsMu.Unlock()
-	msgs, err := rd.ReadMessages()
-	if err != nil {
-		if err == io.EOF {
-			return nil
+	for {
+		msgs, err := rd.ReadMessages()
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
 		}
-		return err
-	}
-	for _, msg := range msgs {
-		if len(msg.Args) == 1 && strings.ToLower(msg.Args[0]) == "quit" {
-			s.monconnsMu.Lock()
-			conn.Write([]byte("+OK\r\n"))
-			s.monconnsMu.Unlock()
-			return nil
+		for _, msg := range msgs {
+			if len(msg.Args) == 1 && strings.ToLower(msg.Args[0]) == "quit" {
+				s.monconnsMu.Lock()
+				conn.Write([]byte("+OK\r\n"))
+				s.monconnsMu.Unlock()
+				return nil
+			}
 		}
+		return nil
 	}
-	return nil
 }
 
 // send messages to live MONITOR clients
