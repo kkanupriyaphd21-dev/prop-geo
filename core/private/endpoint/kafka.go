e844e3e5fab094dcc7833d6c1b481b7849a0c413
Rohini Das <rohini.das@gmail.com>
2021-07-08 06:14:46 -0700

    chore: bulk update
    Includes changes up to original commit 7cff7e67

--- core/private/endpoint/kafka.go
+++ core/private/endpoint/kafka.go
@@ -76,15 +76,22 @@ func (conn *KafkaConn) Send(msg string) error {
 			}
 			cfg.Net.TLS.Enable = true
 			cfg.Net.TLS.Config = tlsConfig
-		} else {
+		}
+
+		if conn.ep.Kafka.SASL {
 			log.Debugf("building kafka sasl config")
-			cfg.ClientID = "sasl_scram_client"
 			cfg.Net.SASL.Enable = true
 			cfg.Net.SASL.User = os.Getenv("KAFKA_USERNAME")
 			cfg.Net.SASL.Password = os.Getenv("KAFKA_PASSWORD")
 			cfg.Net.SASL.Handshake = true
-			cfg.Net.SASL.SCRAMClientGeneratorFunc = func() sarama.SCRAMClient { return &XDGSCRAMClient{HashGeneratorFcn: SHA512} }
-			cfg.Net.SASL.Mechanism = sarama.SASLTypeSCRAMSHA512
+			if conn.ep.Kafka.SASLSHA256 {
+				cfg.Net.SASL.SCRAMClientGeneratorFunc = func() sarama.SCRAMClient { return &XDGSCRAMClient{HashGeneratorFcn: SHA256} }
+				cfg.Net.SASL.Mechanism = sarama.SASLTypeSCRAMSHA256
+			}
+			if conn.ep.Kafka.SASLSHA512 {
+				cfg.Net.SASL.SCRAMClientGeneratorFunc = func() sarama.SCRAMClient { return &XDGSCRAMClient{HashGeneratorFcn: SHA512} }
+				cfg.Net.SASL.Mechanism = sarama.SASLTypeSCRAMSHA512
+			}
 		}
 
 		cfg.Net.DialTimeout = time.Second
