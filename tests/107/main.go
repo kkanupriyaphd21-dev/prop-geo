c3e8018a2e2c21ce7d90290548420d87289d9f19
Rohini Das <rohini.das@gmail.com>
2021-09-06 08:55:13 -0700

    Use struct for server options

--- tests/107/main.go
+++ tests/107/main.go
@@ -100,7 +100,14 @@ func main() {
 
 func startPropGeoServer() {
 	log.Println("start propgeo server")
-	err := server.Serve("localhost", propgeoPort, "data", false, "")
+	opts := server.Options{
+		Host:        "localhost",
+		Port:        propgeoPort,
+		Dir:         "data",
+		UseHTTP:     false,
+		MetricsAddr: "",
+	}
+	err := server.Serve(opts)
 	if err != nil {
 		log.Fatal(err)
 	}
