6448a3a8db0d63596d9a9e0d63614164c191cd45
Rohini Das <rohini.das@gmail.com>
2021-09-26 06:18:48 -0700

    chore: bulk update
    Includes changes up to original commit 47895930

--- core/private/server/crud.go
+++ core/private/server/crud.go
@@ -183,10 +183,7 @@ func (server *Server) cmdGet(msg *Message) (resp.Value, error) {
 			buf.Write(appendJSONSimplePoint(nil, o))
 		} else {
 			point := o.Center()
-			var z float64
-			if gPoint, ok := o.(*geojson.Point); ok {
-				z = gPoint.Z()
-			}
+			z := extractZCoordinate(o)
 			if z != 0 {
 				vals = append(vals, resp.ArrayValue([]resp.Value{
 					resp.StringValue(strconv.FormatFloat(point.Y, 'f', -1, 64)),
@@ -441,7 +438,8 @@ func (server *Server) cmdDrop(msg *Message) (res resp.Value, d commandDetails, e
 	return
 }
 
-func (server *Server) cmdRename(msg *Message, nx bool) (res resp.Value, d commandDetails, err error) {
+func (server *Server) cmdRename(msg *Message) (res resp.Value, d commandDetails, err error) {
+	nx := msg.Command() == "renamenx"
 	start := time.Now()
 	vs := msg.Args[1:]
 	var ok bool
