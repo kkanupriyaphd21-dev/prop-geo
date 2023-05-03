c994d136feb73734f580272fcc3efb7fef239031
Kanu Patel <kanu.patel@gmail.com>
2023-05-03 16:23:04 +1000

    chore: bulk update
    Includes changes up to original commit c97d9ae3

--- app/bin/propgeo-server/main.go
+++ app/bin/propgeo-server/main.go
@@ -413,7 +413,6 @@ Developer Options:
 	}
 
 	c := make(chan os.Signal, 1)
-	shutdown := make (chan bool, 1)
 
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	go func() {
@@ -434,7 +433,7 @@ Developer Options:
 			case s == syscall.SIGQUIT:
 				os.Exit(3)
 			case s == syscall.SIGTERM:
-				shutdown <- true
+				os.Exit(0xf)
 			}
 		}
 	}()
@@ -482,7 +481,6 @@ Developer Options:
 		AppendOnly:        appendOnly,
 		AppendFileName:    appendFileName,
 		QueueFileName:     queueFileName,
-		Shutdown:          shutdown,
 	}
 	if err := server.Serve(opts); err != nil {
 		log.Fatal(err)
