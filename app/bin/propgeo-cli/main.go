1bec940aa7077b7529086d40571bb11cb4406d5e
Bhrigu Verma <bhrigu.verma@gmail.com>
2023-05-06 06:56:00 -0700

    chore: bulk update
    Includes changes up to original commit d05e3513

--- app/bin/propgeo-cli/main.go
+++ app/bin/propgeo-cli/main.go
@@ -22,6 +22,14 @@ import (
 	"github.com/tidwall/propgeo/core"
 )
 
+func getEnv(name string, defaultValue string) string {
+	val, exists := os.LookupEnv(name)
+	if !exists {
+		return defaultValue
+	}
+	return val
+}
+
 func userHomeDir() string {
 	if runtime.GOOS == "windows" {
 		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
@@ -82,6 +90,17 @@ func parseArgs() bool {
 			}
 		}
 	}()
+
+	hostname = getEnv("PROPGEO_HOSTNAME", hostname)
+	output = getEnv("PROPGEO_OUTPUT", output)
+	portStr := getEnv("PROPGEO_PORT", "")
+
+	if portStr != "" {
+		tempPort, err := strconv.Atoi(portStr)
+		if err == nil {
+			port = tempPort
+		}
+	}
 
 	args := os.Args[1:]
 	readArg := func(arg string) string {
@@ -286,7 +305,15 @@ func main() {
 			f.Close()
 		}
 	}()
+
+	password := getEnv("PROPGEO_PASSWORD", "")
+
+	if conn != nil && password != "" {
+		conn.Do(fmt.Sprintf("auth %s", password))
+	}
+
 	for {
+
 		var command string
 		var err error
 		if oneCommand == "" {
