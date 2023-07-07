499c76f3f55397d8483de873652e03192966f4e6
Vishav Sharma <vishav.sharma@gmail.com>
2023-07-06 19:14:28 -0700

    chore: bulk update
    Includes changes up to original commit ef510506

--- core/private/server/pubsub.go
+++ core/private/server/pubsub.go
@@ -205,6 +205,25 @@ func (s *Server) liveSubscription(
 			write([]byte("+OK\r\n"))
 		}
 	}
+	writePing := func() {
+		switch outputType {
+		case JSON:
+			if len(msg.Args) > 1 {
+				write([]byte(`{"ok":true,"ping":` + jsonString(msg.Args[1]) + `,"elapsed":"` + time.Since(start).String() + `"}`))
+			} else {
+				write([]byte(`{"ok":true,"ping":"pong","elapsed":"` + time.Since(start).String() + `"}`))
+			}
+		case RESP:
+			if len(msg.Args) > 1 {
+				data := redcon.AppendArray(nil, 2)
+				data = redcon.AppendBulkString(data, "PONG")
+				data = redcon.AppendBulkString(data, msg.Args[1])
+				write(data)
+			} else {
+				write([]byte("+PONG\r\n"))
+			}
+		}
+	}
 	writeWrongNumberOfArgsErr := func(command string) {
 		switch outputType {
 		case JSON:
@@ -335,6 +354,9 @@ func (s *Server) liveSubscription(
 			case "quit":
 				writeOK()
 				return nil
+			case "ping":
+				writePing()
+				continue
 			case "psubscribe":
 				kind, un = pubsubPattern, false
 			case "punsubscribe":
